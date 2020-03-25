a1b3dfbb400c36083e5c9c4a24de21816a319ae6
Khaled Adad <khaledadad@lewisu.edu>
2020-03-24 22:54:30 -0500

    Building Business component

--- ravenous_app/src/components/Business/Business.js
+++ ravenous_app/src/components/Business/Business.js
@@ -1,1 +1,24 @@
-impor React from 'react';
+import React, { Component } from "react";
+
+const business = {
+  imageSrc:
+    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+  name: "MarginOtto Pizzeria",
+  address: "1010 Paddington Way",
+  city: "Flavortown",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
+  rating: 4.5,
+  reviewCount: 90
+};
+
+class Business extends React.Component {
+  render () {
+    return (
+      
+    )
+  }
+}
+
+export default Business;
