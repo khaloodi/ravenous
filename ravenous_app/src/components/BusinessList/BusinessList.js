79edc109f98080f7e1b3a23215a0bb25b87de489
Khaled Adad <khaledadad@lewisu.edu>
2020-03-24 23:26:46 -0500

    Editing BusinessList and styles

--- ravenous_app/src/components/BusinessList/BusinessList.js
+++ ravenous_app/src/components/BusinessList/BusinessList.js
@@ -1,1 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
+import "./BusinessList.css";
+import Business from "../Business/Business";
+
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    );
+  }
+}
+
+export default BusinessList;
