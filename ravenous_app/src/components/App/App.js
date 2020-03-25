9ed352e8ae3aab146b9cde2eddc07f12459dddc4
Khaled Adad <khaledadad@lewisu.edu>
2020-03-24 23:19:30 -0500

    Started BusinessList component and added some styling

--- ravenous_app/src/components/App/App.js
+++ ravenous_app/src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import "../Business/Business";
+import Business from "../Business/Business";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Business />
     </div>
   );
 }
