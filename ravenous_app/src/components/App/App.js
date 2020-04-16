6e4ec8f40b891c3894064363afa2439a47c248a1
Khaled Adad <khaledadad@lewisu.edu>
2020-04-15 22:38:58 -0500

    move business object to app.js

--- ravenous_app/src/components/App/App.js
+++ ravenous_app/src/components/App/App.js
@@ -4,13 +4,28 @@ import "../Business/Business";
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 
+const business = {
+    imageSrc: "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+    name: "MarginOtto Pizzeria",
+    address: "1010 Paddington Way",
+    city: "Flavortown",
+    state: "NY",
+    zipCode: "10101",
+    category: "Italian",
+    rating: 4.5,
+    reviewCount: 90,
+};
+
 function App() {
-  return (
-    <div className="App">
-      <h1> ravenous </h1> <SearchBar />
-      <BusinessList />
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        h1 > ravenous < /h1> <SearchBar / >
+        <
+        BusinessList / >
+        <
+        /div>
+    );
 }
 
 export default App;
