f733f279212370584fe0a4c27e5328478797a62e
Khaled Adad <khaledadad@lewisu.edu>
2020-11-30 18:52:58 -0600

    Officially deployed to netlify

---
+++ build/precache-manifest.88d78fd49bf02973e4487b116953c4bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eee03c5ca2140e39d11376c7ad8438f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c6d0a6d8bacf88936d9",
+    "url": "/static/css/main.0408044c.chunk.css"
+  },
+  {
+    "revision": "5b70999693e88565bbf8",
+    "url": "/static/js/2.2a17b70c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2a17b70c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6d0a6d8bacf88936d9",
+    "url": "/static/js/main.329fa63c.chunk.js"
+  },
+  {
+    "revision": "7cdab48631107edc87ed",
+    "url": "/static/js/runtime-main.a7fec06d.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
